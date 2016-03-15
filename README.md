f125ee0a717ccca2cd6730becaef6bd662ddf61a
Feliciano Santana <fsfelicianosantana@gmail.com>
2016-03-15 20:36:38 -0300

    Nests template inside JS

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Angular wrapper around [Codrops angular thumbnail grid](http://tympanus.net/codrops/?p=14530https://github.com/username/repo/blob/branch/docs/more_words.md).
 
 Original code by Codrops
+
 Angular wrapper by Flic
 
 Please read about Codrops license: http://tympanus.net/codrops/licensing/
--- js/angular-thumbnail-grid.js
+++ js/angular-thumbnail-grid.js
@@ -8,7 +8,7 @@
 			bindToController: {
 				images: '='
 			},
-			templateUrl: 'ogThumbnailGrid.html',
+			template: '<ul id="og-grid" class="og-grid"><li ng-repeat="image in vm.images"><a href="#" data-largesrc="{{image.largesrc}}" data-title="{{image.title}}" data-description="{{image.description}}" data-details="{{image.template}}"><img ng-src="{{image.thumbnailsrc}}" alt="img01"/></a></li></ul>',
 			link: function (scope, elm, attr) {
 				$timeout(function () {
 					Grid.addItems(angular.element(elm.children()[0]).children());
